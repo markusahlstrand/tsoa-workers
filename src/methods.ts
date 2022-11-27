0208e6a47106ab177070c84cc60033dae1ba95dd
Markus Ahlstrand <markus@ahlstrand.de>
2022-11-27 10:19:57 +0100

    Ma/v1 (#1)
    * feat: first working version
    
    * chore: remove node_modules
    
    * chore: add gitignore

---
+++ src/methods.ts
@@ -0,0 +1,55 @@
+export function Options(value?: string) {
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {};
+}
+
+export function Get(value?: string) {
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {};
+}
+
+export function Post(value?: string) {
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {};
+}
+
+export function Put(value?: string) {
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {};
+}
+
+export function Patch(value?: string) {
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {};
+}
+
+export function Delete(value?: string) {
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {};
+}
+
+export function Head(value?: string) {
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {};
+}
